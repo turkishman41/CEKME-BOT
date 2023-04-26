faea67dabeb8e9caa8ab06315742bd4b309ab8be
turkishman41 <89349479+turkishman41@users.noreply.github.com>
2023-04-26 20:46:20 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,18 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 1317481
-API_HASH = "e5f7a25123dca9d734516cde8ae4c374"
-BOT_TOKEN = "5947534274:AAEmY-91HrtD2sYSiinRIyB6rY1BxOU5bXk"
-SESSION = "BAADr_ipQPv4DDxNKzZoXYGTH9ooG71ILy03fQnPV9D1XZmxjTgxrwy3er_BObfxJpSSV6eAm-iXjt2797WE89DeDCrpPdogWAkeycmm6I2qQf35T-K7V_2p_rjEv3yqR_emP9HzKgSxbkUoi7ViwEp-dUHIYLJDDuHCuO864vnJK6UOilsVYqFE_g0H1XrXMYLO9Ov1ufiQV8eJxEmCdmn5oL-XMh2wAbVlcBnWvOdS5MGNa4xggAAL4e0aGweDU-DIZdFy-uVP5eqN6ywFl7v7EHXL0j4ebVjeHaUprA2zz0HlRULKVmqUfLBJj8sAz582q-GoE3m-LAyG69SLQ2CKAAAAAV2IWkgA" 
-FORCESUB = "DTO_bots"
-AUTH = 965221088
+
+API_ID = config("API_ID", default=None, cast=int)
+
+API_HASH = config("API_HASH", default=None)
+
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+SESSION = config("SESSION", default=None)
+
+FORCESUB = config("FORCESUB", default=None)
+
+AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
